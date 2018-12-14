d3a4440076512f56ae422776bab63bd47ed1ca22
Eric Van Der Dijs <eric2vanderdijs@gmail.com>
2018-12-13 23:50:21 -0400

    code challenge completed

--- app/src/main/java/com/example/pipboy2300/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/pipboy2300/hellotoast/MainActivity.java
@@ -14,17 +14,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if(savedInstanceState != null){
+            mCount = Integer.parseInt( savedInstanceState.getString("mCount") );
+        }
+
         setContentView(R.layout.activity_main);
         mCountView = (TextView) findViewById(R.id.count_text);
+        mCountView.setText( Integer.toString(mCount) );
     }
 
-    public void showToast(View view) {
+    @Override
+    protected void onSaveInstanceState (Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("mCount", mCountView.getText().toString() );
+    }
+
+    public void countUp(View view) {
         mCount++;
         if( mCountView != null )
             mCountView.setText( Integer.toString(mCount) );
     }
 
-    public void countUp(View view) {
+    public void showToast(View view) {
         Toast toast = Toast.makeText(this, R.string.toast_mesage, Toast.LENGTH_LONG);
 
         toast.show();
